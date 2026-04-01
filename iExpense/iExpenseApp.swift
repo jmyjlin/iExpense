ddb69d3768e91d716ac10720e2c9bddce197fe26
Jimmy Lin <jmyjlin@gmail.com>
2026-04-01 12:30:13 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jimmy Lin on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
